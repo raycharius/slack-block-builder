f91c0e14c63daf889cb2502219f2e2c4ccc6e871
Ray East <raydeast@gmail.com>
2020-11-09 00:51:53 +0200

    :fire: Remove unnecessary console.logs from tests

--- tests/checks/mutated-build.js
+++ tests/checks/mutated-build.js
@@ -7,11 +7,7 @@ module.exports = ({ Class, method, property, param, valid, mutated, expectArray 
     const built = instance.build({ raw: true });
 
     if (expectArray) {
-      console.log('IN EXPECT ARRAY');
       instance.result[property].forEach((value, i) => {
-        console.log(property);
-        console.log(instance.result[property][i]);
-        console.log(mutated);
         expect(instance.result[property][i]).toBeInstanceOf(mutated);
         expect(built[param][i]).toEqual(instance.result[property][i]);
       });
