00f182494b676ef84bdd1798d1e2b4204d63b132
Ray East <raydeast@gmail.com>
2021-05-28 23:09:48 +0300

    :bulb: Document Md helper functions with JSDoc

--- src/md/index.ts
+++ src/md/index.ts
@@ -1,38 +1,82 @@
+/**
+ * @description Wraps a string in quotation marks.
+ */
+
 export function quote(string: string): string {
   return `"${string}"`;
 }
+
+/**
+ * @description Makes a string a block quote.
+ */
 
 export function blockquote(string: string): string {
   return `>${string}`;
 }
 
+/**
+ * @description Makes a string bold.
+ */
+
 export function bold(string: string): string {
   return `*${string}*`;
 }
+
+/**
+ * @description Makes a string italic.
+ */
 
 export function italic(string: string): string {
   return `_${string}_`;
 }
 
+/**
+ * @description Strikes out a string.
+ */
+
 export function strike(string: string): string {
   return `~${string}~`;
 }
 
-export function code(string: string): string {
+/**
+ * @description Turns a string into an inline block of code.
+ */
+
+export function codeInline(string: string): string {
   return `\`${string}\``;
 }
 
-export function listDash(...strings: Array<string | string[]>): string {
-  return strings.flat()
+/**
+ * @description Turns a string into a multi-line block of code.
+ */
+
+export function codeBlock(string: string): string {
+  return `\`\`\`${string}\`\`\``;
+}
+
+/**
+ * @description Formats multiple strings into a dashed list.
+ */
+
+export function listDash(...items: Array<string | string[]>): string {
+  return items.flat()
     .map((string) => `- ${string}`)
     .join('\n');
 }
 
-export function listBullet(...strings: Array<string | string[]>): string {
-  return strings.flat()
+/**
+ * @description Formats multiple strings into a bulleted list.
+ */
+
+export function listBullet(...items: Array<string | string[]>): string {
+  return items.flat()
     .map((string) => `• ${string}`)
     .join('\n');
 }
+
+/**
+ * @description Formats a URL into a clickable, aliased link.
+ */
 
 export function link(url: string, alias?: string): string {
   return alias
@@ -40,20 +84,36 @@ export function link(url: string, alias?: string): string {
     : `<${url}>`;
 }
 
+/**
+ * @description Formats an email address into a clickable link.
+ */
+
 export function mailto(email: string, alias: string): string {
   return `<mailto:${email}|${alias}>`;
 }
 
-export function user(string: string): string {
-  return `<@${string}>`;
+/**
+ * @description Mentions a user in a channel.
+ */
+
+export function user(id: string): string {
+  return `<@${id}>`;
 }
 
-export function channel(string: string): string {
-  return `<#${string}>`;
+/**
+ * @description Creates a clickable link to a channel.
+ */
+
+export function channel(id: string): string {
+  return `<#${id}>`;
 }
 
-export function group(string: string): string {
-  return `<!subteam^${string}>`;
+/**
+ * @description Mentions a Slack user group.
+ */
+
+export function group(id: string): string {
+  return `<!subteam^${id}>`;
 }
 
 const md = {
@@ -62,7 +122,8 @@ const md = {
   bold,
   italic,
   strike,
-  code,
+  codeInline,
+  codeBlock,
   listDash,
   listBullet,
   link,
