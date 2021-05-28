f93f740e045413c56d5ad14e7e64a03462dfead0
Ray East <raydeast@gmail.com>
2021-05-28 23:20:11 +0300

    :bulb: Add JSDoc to utility functions

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,18 +1,24 @@
+/* eslint-disable max-len */
+
 import { getBuiltCollection } from '../helpers';
 
 import type { BlockBuilder, Appendable } from '../types';
 import type { AttachmentBuilder } from '../bits';
 import type { SlackBlockDto, SlackDto } from '../lib';
 
-export function BlockCollection(
-  ...blocks: Appendable<BlockBuilder>
-): Readonly<SlackBlockDto>[] {
+/**
+ * @description Creates and returns an array of built blocks. Behaves in the same way as all appending methods, such as Surface.blocks().
+ */
+
+export function BlockCollection(...blocks: Appendable<BlockBuilder>): Readonly<SlackBlockDto>[] {
   return getBuiltCollection<BlockBuilder, Readonly<SlackBlockDto>>(...blocks);
 }
 
-export function AttachmentCollection(
-  ...attachments: Appendable<AttachmentBuilder>
-): Readonly<SlackDto>[] {
+/**
+ * @description Creates and returns an array of built attachments. Behaves in the same way as all appending methods, such as Message.attachments().
+ */
+
+export function AttachmentCollection(...attachments: Appendable<AttachmentBuilder>): Readonly<SlackDto>[] {
   return getBuiltCollection<AttachmentBuilder, Readonly<SlackDto>>(...attachments);
 }
 
