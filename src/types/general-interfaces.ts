0bd01f986f3282ee11cfb07c7f35e2f22809ee35
Ray East <raydeast@gmail.com>
2021-07-24 13:53:43 +0300

    :hammer: Refactor undefinable types for strict mode

--- src/types/general-interfaces.ts
+++ src/types/general-interfaces.ts
@@ -9,3 +9,7 @@ export type Ctor<T = Record<string, unknown>> = new (...args: any[]) => T;
 export type AbstractCtor<T> = { prototype: T };
 
 export type Appendable<T> = Array<T | T[]>;
+
+export type Undefinable<T> = T | undefined;
+
+export type UndefinableArray<T> = Undefinable<T>[];
